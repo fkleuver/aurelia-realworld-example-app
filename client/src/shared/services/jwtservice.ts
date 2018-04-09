7f252a8892f862b870f09c4fc2d207733d2180ba
Fred Kleuver <fred@avurad.nl>
2018-04-09 06:56:44 +0200

    refactor(jwtservice): make token storage a bit more robust

--- client/src/shared/services/jwtservice.ts
+++ client/src/shared/services/jwtservice.ts
@@ -1,13 +1,17 @@
+const storageKey = "aurelia-realworld-example-app:jwtToken";
+
 export class JwtService {
   public getToken(): string {
-    return window.localStorage.jwtToken;
+    return window.localStorage.getItem(storageKey) as any;
   }
 
   public saveToken(token: string): void {
-    window.localStorage.jwtToken = token;
+    if (typeof token === "string") {
+      window.localStorage.setItem(storageKey, token);
+    }
   }
 
   public destroyToken(): void {
-    window.localStorage.removeItem("jwtToken");
+    window.localStorage.removeItem(storageKey);
   }
 }
