1a6a6b7d9f044272d2af2473acb5926ef586e3b5
Fred Kleuver <fred@avurad.nl>
2018-04-09 06:55:10 +0200

    fix(servicehelper): correctly handle other error types

--- client/src/shared/services/servicehelper.ts
+++ client/src/shared/services/servicehelper.ts
@@ -7,9 +7,15 @@ export function status<T>(response: Response): T {
 }
 
 export function parseError(error: any): any {
-  if (!(error instanceof Error)) {
+  if (error instanceof Error) {
+    return error;
+  }
+  if (error instanceof Response) {
+    return new Error(error.statusText);
+  }
+  if (typeof error.json === "function") {
     return new Promise<any>((_resolve, reject) => reject(error.json()));
   }
 
-  return error;
+  return new Promise<any>((_resolve, reject) => reject(error));
 }
